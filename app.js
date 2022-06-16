ee17c2d5ba08e761d01f1a139afce85b2949ea43
José Enrique <otipip90@gmail.com>
2022-06-15 23:41:48 -0500

    control menu flow

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 require('colors');
 const { showMessage, pause } = require('./helpers/messages');
-const main = async () =>{
-    
+const main = async () => {
+    let option = '';
+    do{
+        option = await showMessage();
+        await pause();
+    }while(option !== '0');
     showMessage('Hola Mundo');
     pause()
 }
--- helpers/messages.js
+++ helpers/messages.js
@@ -1,38 +1,45 @@
 require('colors');
 
 const showMessage = () => {
-    console.clear();
-    console.log('============================');
-    console.log('    Select option'.green);
-    console.log('============================\n');
-    console.log(`${'1.'.green} Create task`);
-    console.log(`${'2.'.green} List task`);
-    console.log(`${'3.'.green} List complete task`);
-    console.log(`${'4.'.green} List pending task`);
-    console.log(`${'5.'.green} Complete task(s)`);
-    console.log(`${'6.'.green} Remove task`);
-    console.log(`${'0.'.red} Exit`);
-    
-    const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+    return new Promise(resolve => {
+        console.clear();
+        console.log('============================');
+        console.log('    Select option'.green);
+        console.log('============================\n');
+        console.log(`${'1.'.green} Create task`);
+        console.log(`${'2.'.green} List task`);
+        console.log(`${'3.'.green} List complete task`);
+        console.log(`${'4.'.green} List pending task`);
+        console.log(`${'5.'.green} Complete task(s)`);
+        console.log(`${'6.'.green} Remove task`);
+        console.log(`${'0.'.red} Exit`);
+        
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
 
-    readline.question('Select option: ', (option) => {
-        console.log({option})
-        readline.close();}
-    );
+        readline.question('Select option: ', (option) => {
+            console.log({option})
+            resolve(option);
+            readline.close();}
+        );
+    });
 }
 
 const pause = () => {
+    return new Promise(resolve => {
+
     const readline = require('readline').createInterface({
         input: process.stdin,
         output: process.stdout
     });
     readline.question(`\nPress ${'Enter'.green } to continue` , (option) => {
         readline.close();
+        resolve();
         }
     );
+    });
 }
 module.exports = {
     showMessage,
